42de643ca9f39656b883b5dbd8eab7da1056b9f3
Martinez-LMS <leo.ooosla@gmail.com>
2024-07-11 11:19:43 -0300

    finalizando

---
+++ NomeDoProjeto/Models/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Leonardo.Models;
+
+public class AppDataContext : DbContext
+{
+    public DbSet<Usuario> Usuario { get; set; }
+    public DbSet<imc> imc { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=leonardo.db");
+    }
+}
